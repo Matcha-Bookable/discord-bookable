68807a25af72d94c318a0e80a7b46d8d10542ad9
avan <leechunyin2004@gmail.com>
2026-01-28 10:08:48 +0000

    updated reminder message

--- main.py
+++ main.py
@@ -436,7 +436,7 @@ async def sendServerDetails(userid: int, msg: WebhookMessage, details: dict):
     instanceName = details["instance"]
     region = f"{g_regions[details["region"]]["fullname"]} ({details["region"].upper()})"
     # Should probably turn this into an environment variable instead in the future
-    reminder = "Please enter the server first to obtain the host status.\nUse `!host` as the host to control the functionalities of the server (Maps, Config etc).\n\nUse `!votemenu` to change configs and maps.\nUse `!sdr` to receive SDR connect string in-game.\nServer will close if there are less than 2 players for 10 minutes."
+    reminder = "Please enter the server first to obtain the host status.\nUse `!host` as the host to control the functionalities of the server (Maps, Config etc).\n\nUse `!votemenu` to change configs and maps.\nUse `!sdr` to receive SDR connect string in-game.\nServer will close if there are less than 2 players for 4 minutes."
 
     # Send DM embed to user
     embed_dm = Embed(
